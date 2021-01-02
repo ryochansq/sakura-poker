bc811020ffa7017daab41e3a9772c14afa4d7b0e
ryochansq <nicestsnsq@gmail.com>
2021-01-02 18:22:53 +0900

    layout & draw function

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,40 +1,62 @@
 import React from 'react'
-import Link from 'next/link'
 import Head from 'next/head'
+import { AppBar, Button, Grid, Link, Paper, Toolbar, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  paper: {
+    padding: '16px 8px'
+  },
+  footer: {
+    marginTop: 16,
+  }
+})
 
 type Props = {
   title?: string
 }
 
-const Layout :React.FC<Props> = ({ children, title = 'Poker' }) => (
+const Layout: React.FC<Props> = ({ children, title = 'さくら学院ポーカー' }) => {
+  const classes = useStyles()
+
+  return (
     <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
-)
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <style jsx global>{`
+      body {
+        margin: 0px;
+      }
+    `}</style>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6' style={{ flexGrow: 1 }}>さくら学院ポーカー</Typography>
+          <Button color="inherit">ヒント</Button>
+        </Toolbar>
+      </AppBar>
+      <Grid container justify='center'>
+        <Grid item xs={12} md={8} lg={6}>
+          <Paper className={classes.paper}>
+            {children}
+            <Grid container spacing={1} className={classes.footer}>
+              <Grid item container justify='flex-end'>
+                <Typography variant='caption'>
+                  Created by{' '}
+                  <Link href='https://twitter.com/ryochan_metal' target='_blank'>@ryochan_metal</Link>
+                </Typography>
+              </Grid>
+              <Grid item container justify='flex-end'>
+                <Typography variant='caption'>推奨ブラウザ：Chrome, Safari</Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
 
 export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,37 @@
-import Link from 'next/link'
+import React from 'react'
+import { Button } from '@material-ui/core'
+
 import Layout from 'components/Layout'
+import { members, Member } from 'interfaces/index'
 
-const IndexPage = () => (
-  <Layout>
-    <h1>Hello Poker 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+  let defaultDeck = [...members] as Member[]
+  if (Math.random() * 10 > 1) defaultDeck.pop() // 10%の確率で森ハヤシがpopされない
+  for (let i = defaultDeck.length - 1; i > 0; i--) {  // 山札をシャッフル
+    let r = Math.floor(Math.random() * (i + 1))
+    let tmp = defaultDeck[i]
+    defaultDeck[i] = defaultDeck[r]
+    defaultDeck[r] = tmp
+  }
+  const [deck, setDeck] = React.useState<Member[]>(defaultDeck)
+  const [hand, setHand] = React.useState<Member[]>([])
 
+  const draw = (num: number) => {
+    let newDeck = [...deck]
+    let newHand = [...hand]
+    for (let i = 0; i < num; i++) {
+      const card = newDeck.pop()
+      if (card) newHand.push(card)
+    }
+    setDeck(newDeck)
+    setHand(newHand)
+  }
+  React.useEffect(() => draw(5), [])  // created
+  return (
+    <Layout>
+      <h1>Hello Poker 👋</h1>
+      <Button onClick={() => draw(1)} variant="contained">hoge</Button>
+    </Layout>
+  )
+}
 export default IndexPage
